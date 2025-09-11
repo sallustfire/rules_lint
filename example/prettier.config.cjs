94ef0bf95a09dff154e3434c88f109f7802a44ba
Connor McEntee <sallustfire@gmail.com>
2025-09-11 13:13:51 -0600

    Demo broken prettier config with slash pattern

--- example/prettier.config.cjs
+++ example/prettier.config.cjs
@@ -5,10 +5,22 @@ const config = {
   tabWidth: 2,
   printWidth: 80,
   plugins: [
-    require("prettier-plugin-sql"),
+    // require("prettier-plugin-sql"),
     require("@prettier/plugin-xml"),
     require("prettier-plugin-gherkin"),
   ],
+  overrides: [
+    {
+      files: ["**/*.sql"],
+      // For my local config this is the minimal pattern that works
+      // files: ["../../../../../../../../../../../../../**/*.sql"],
+      options: {
+        parser: 'sql',
+        language: 'postgresql',
+        plugins: [require("prettier-plugin-sql"),],
+      },
+    }
+  ]
 };
 
 module.exports = config;
